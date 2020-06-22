b4270b71acc70ea1178a26d86ea3eadc0d780926
Riryan <anthonyfmitchell@gmail.com>
2020-06-22 15:09:37 -0400

    Add files via upload

---
+++ rdx_skin/fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'adamant'
+
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'RDX Skin'
+
+version '1.0.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@redm_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@redm_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'redm_extended',
+	'skinchanger'
+}
